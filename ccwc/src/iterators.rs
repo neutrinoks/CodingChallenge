1e4f7805edd46b111fe332cb488807d6c727d4b0
Michael Eberhardt <github@michael-eberhardt.de>
2023-10-08 11:07:51 +0400

    word iterator is working properly

---
+++ ccwc/src/iterators.rs
@@ -0,0 +1,72 @@
+//! Module encapsules individual iterator implementations.
+
+use std::str::CharIndices;
+
+
+/// Iterator for extracting words out of a text properly.
+#[derive(Clone, Debug)]
+pub struct WordIterator<'r> {
+    /// Source text to be iterated.
+    text: &'r str,
+    /// Internal iterator.
+    iter: CharIndices<'r>,
+}
+
+impl<'r> WordIterator<'r> {
+    pub fn new(text: &'r str) -> WordIterator<'r> {
+        let iter = text.char_indices();
+        WordIterator{ text, iter }
+    }
+}
+
+impl<'r> Iterator for WordIterator<'r> {
+    type Item = &'r str;
+
+    fn next(&mut self) -> Option<&'r str> {
+        let mut set = false;
+        let mut start = 0;
+
+        // Step 1: Search for next beginning word.
+        while let Some((i,c)) = self.iter.next() {
+            if c.is_alphanumeric() {
+                start = i;
+                set = true;
+                break;
+            }
+        }
+        if !set {
+            return None
+        }
+
+        // Step 2: Search for end of this word.
+        let mut stop = start;
+        while let Some((i,c)) = self.iter.next() {
+            stop = i;
+            if !(c.is_alphanumeric() || c == '-' || c == '.') {
+                break;
+            }
+        }
+
+        Some(&self.text[start..stop])
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn worditer_simple_line() {
+        let text: &str = "This is a simple, single line of text.";
+        let mut iter = WordIterator::new(text);
+        assert_eq!(iter.count(), 8);
+    }
+
+    #[test]
+    fn worditer_special_characters() {
+        let text: &str = "\u{feff}This is a simple,\nvery simple\t line of text.";
+        let mut iter = WordIterator::new(text);
+        assert_eq!(iter.count(), 9);
+    }
+}
